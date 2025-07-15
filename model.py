97e51e3ba7fb52c1575e13ee63ac69827e10a148
Aliza-Akhtar22 <akhtaraliza120@gmail.com>
2025-07-15 14:02:39 +0530

    Modified

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ class CourtData(Base):
     __tablename__ = "courts_data"
 
     id = Column(Integer, primary_key=True, index=True)
-    ds = Column(String, nullable=False)  # Date as string; will parse in code
+    ds = Column(String, nullable=False)  
     institution = Column(Integer)
     disposal = Column(Integer)
     y = Column(Integer)
